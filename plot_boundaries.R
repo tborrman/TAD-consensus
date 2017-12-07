cdd3cda627a5ad48531caa7d96dd26caa233be34
tborrman <tylerborrman@gmail.com>
2017-12-06 20:58:32 -0500

    color

--- plot_boundaries.R
+++ plot_boundaries.R
@@ -15,40 +15,61 @@ get_conservation <- function(i, df) {
 	# Return conservation for start 
 	# and end boundaries in row i of df
 
-	# R1 : cyan
-	# R2 : dodgerblue
-	# P: steelblue1
-	# R1,R2: mediumorchid2
-	# R1,P: purple
-	# R2,P: magenta2
-	# R1,R2,P: red
-
 	# Get block of replicates br
 	cellname <- df[i,"cell"]
 	br <- df[df$cell == cellname,]
 	# Get conservaion of start boundary
 	boundary_start <- df[i, "TADstart"]
 	conserved <- br[br$TADstart == boundary_start | br$TADend == boundary_start,]
-	if (nrow(conserved) > 3) {
-		print(conserved)
-		print('ERROR: more than 3 replicate types')
-		quit()
-	}
-	cons_rep_start <- as.character(conserved$biorep)
+	# if (nrow(conserved) > 3) {
+	# 	print(conserved)
+	# 	print('ERROR: more than 3 replicate types')
+	# }
+	cons_rep_start <- levels(as.factor(as.character(conserved$biorep)))
 	cons_rep_start <- paste(cons_rep_start[order(cons_rep_start)], collapse="_")
+
 	# Get conservaion of end boundary
 	boundary_end <- df[i, "TADend"]
 	conserved <- br[br$TADstart == boundary_end | br$TADend == boundary_end,]
-	if (nrow(conserved) > 3) {
-		print(conserved)
-		print('ERROR: more than 3 replicate types')
-		quit()
-	}	
-	cons_rep_end <- as.character(conserved$biorep)
+	cons_rep_end <- levels(as.factor(as.character(conserved$biorep)))
 	cons_rep_end <- paste(cons_rep_end[order(cons_rep_end)], collapse="_")
 
 	return(c(cons_rep_start, cons_rep_end))
 }
+
+add_colors <- function(df) {
+	# Add conservation colors to dataframe
+	# P: steelblue1
+	# R1 : cyan
+	# R2 : dodgerblue
+	# R1_R2: mediumorchid2
+	# P_R1: purple
+	# P_R2: magenta2
+	# P_R1_R2: red
+	
+	color_map <- c("steelblue1", "cyan", "dodgerblue",
+		"mediumorchid2", "purple", "magenta2", "red")  
+	names(color_map) <- c("P", "R1", "R2", "R1_R2", "P_R1",
+		"P_R2", "P_R1_R2")
+
+	constart <- df$cons_start
+	consend <- df$cons_end
+	# Order errors when trying to use simple indexing or sapply
+	# Using stupid loops instead, I hate R
+	cons_start_color <- c()
+	cons_end_color <- c()
+	for (cst in constart) {
+		cons_start_color <- c(cons_start_color, color_map[cst])
+	}
+	for (csd in consend) {
+		cons_end_color <- c(cons_end_color, color_map[csd])
+	}
+	cdf <- cbind(df, cons_start_color, cons_end_color)
+	return(cdf)
+}
+
+
+
 # Build Hi-C TAD dataframe
 for (i in 1:nrow(hic_names)) {
 	if (!exists("hic_df")) {
@@ -74,6 +95,8 @@ for (i in 1:nrow(hic_names)) {
 	}	
 }
 
+
+
 # Subtract 1 bp from TADstart for matching conservation 
 # between boundaries at start and end of TAD
 hic_df$TADstart <- hic_df$TADstart - 1
@@ -83,7 +106,7 @@ hic_df$TADstart <- hic_df$TADstart - 1
 cons_start <- c();
 cons_end <- c();
 for (i in 1:nrow(hic_df)){
-	if (i%%1000 == 0) {
+	if (i%%250 == 0) {
 		print(i)
 	}
 	clist <- get_conservation(i, hic_df)
@@ -92,4 +115,8 @@ for (i in 1:nrow(hic_df)){
 }
 
 hic_df <- cbind(hic_df, cons_start, cons_end)
-head(hic_df,2000)
+hic_df <- add_colors(hic_df)
+write.table(hic_df, paste("plot_boundaries_table_", args$c, ".txt", sep=""), 
+	sep="\t", col.names=TRUE, quote=FALSE, row.names=FALSE )
+
+
